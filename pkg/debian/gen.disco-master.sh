e9076563ee22170f1516fd4608ebb90be355f2ca
Mark Williams <markrwilliams@gmail.com>
2015-05-12 09:53:39 -0700

    use ssh-agent to run the disco master

--- pkg/debian/gen.disco-master.sh
+++ pkg/debian/gen.disco-master.sh
@@ -23,7 +23,7 @@ running() {
 }
 
 start_server() {
-        su - disco --command="${RELBIN}/disco start"
+        su - disco --command="/usr/bin/ssh-agent ${RELBIN}/disco start"
         errcode=\$?
 	return \$errcode
 }
